25bc6aafce54bcda6249ac233c11870adbc4527c
tatsuya730 <t.taniguchi730@gmail>
2023-11-12 05:11:50 +0900

    価格ページ修正

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -1,76 +1,40 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>価格ページ</title>
-    <!-- BootstrapのCSSファイルへのリンクを挿入します -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div class="container mt-5">
-        <h1 class="text-center mb-4">価格表</h1>
-        <div class="row">
-            <!-- ベーシックプラン -->
-            <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                    <div class="card-header">
-                        <h4 class="my-0 font-weight-normal">ベーシック</h4>
-                    </div>
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">¥1,000 <small class="text-muted">/ 月</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>10ユーザーまで</li>
-                            <li>2 GBのストレージ</li>
-                            <li>メールサポート</li>
-                            <li>ヘルプセンターアクセス</li>
-                        </ul>
-                        <button type="button" class="btn btn-lg btn-block btn-outline-primary">サインアップ</button>
-                    </div>
-                </div>
-            </div>
-            <!-- プロプラン -->
-            <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                    <div class="card-header">
-                        <h4 class="my-0 font-weight-normal">プロ</h4>
-                    </div>
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">¥2,000 <small class="text-muted">/ 月</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>20ユーザーまで</li>
-                            <li>10 GBのストレージ</li>
-                            <li>優先メールサポート</li>
-                            <li>ヘルプセンターアクセス</li>
-                        </ul>
-                        <button type="button" class="btn btn-lg btn-block btn-primary">開始</button>
-                    </div>
-                </div>
-            </div>
-            <!-- エンタープライズプラン -->
-            <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                    <div class="card-header">
-                        <h4 class="my-0 font-weight-normal">エンタープライズ</h4>
-                    </div>
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">¥3,000 <small class="text-muted">/ 月</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>50ユーザーまで</li>
-                            <li>30 GBのストレージ</li>
-                            <li>電話およびメールサポート</li>
-                            <li>ヘルプセンターアクセス</li>
-                        </ul>
-                        <button type="button" class="btn btn-lg btn-block btn-primary">お問い合わせ</button>
-                    </div>
-                </div>
+<x-app-layout>
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <div class="flex flex-col mt-8">
+            <div class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead>
+                        <tr>
+                            <th class="px-6 py-3 bg-gray-50 text-left text-base leading-4 font-medium text-gray-500 uppercase tracking-wider">プラン</th>
+                            <th class="px-6 py-3 bg-gray-50 text-right text-base leading-4 font-medium text-gray-500 uppercase tracking-wider">価格</th>
+                            <th class="px-6 py-3 bg-gray-50"></th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap text-base leading-5 font-medium text-gray-900">1回生成</td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-base leading-5 text-gray-500 text-right">¥10,000</td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-right text-base leading-5 font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900 font-semibold">100コインチャージ</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap text-base leading-5 font-medium text-gray-900">3回生成</td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-base leading-5 text-gray-500 text-right">¥20,000</td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-right text-base leading-5 font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900 font-semibold">300コインチャージ</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap text-base leading-5 font-medium text-gray-900">10回生成</td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-base leading-5 text-gray-500 text-right">¥50,000</td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-right text-base leading-5 font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900 font-semibold">1000コインチャージ</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-    <!-- オプショナルのJavaScript -->
-    <!-- 最初にjQuery、次にPopper.js、次にBootstrap JS -->
-    <script src="{{ asset('js/jquery-3.5.1.slim.min.js') }}"></script>
-    <script src="{{ asset('js/popper.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-</body>
-</html>
+</x-app-layout>
