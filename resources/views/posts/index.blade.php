8c84602c697b7ef659ac2a5af4c5f9a25be0ce6c
tatsuya730 <t.taniguchi730@gmail>
2023-11-12 11:05:56 +0900

    記事のユーザー表示を消した

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,10 +8,10 @@
                 <article class="w-full px-4 md:w-1/2 text-xl text-gray-800 leading-normal">
                     <a href="{{ route('posts.show', $post) }}">
                         <h2 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-1 text-3xl md:text-4xl break-words">{{ $post->title }}</h2>
-                        <h3>{{ $post->user->name }}</h3>
+                        <!-- 削除またはコメントアウト: <h3>{{ $post->user->name }}</h3> -->
                         <p class="text-sm mb-2 md:text-base font-normal text-gray-600">
                             <span class="text-red-400 font-bold">{{ date('Y-m-d H:i:s', strtotime('-1 day')) < $post->created_at ? 'NEW' : '' }}</span>
-                            {{ $post->created_at }}
+                            {{ $post->created_at->format('Y-m-d H:i') }} <!-- 日付のフォーマットを修正 -->
                         </p>
                         <img class="w-full mb-2" src="{{ $post->image_url }}" alt="">
                         <p class="text-gray-700 text-base">{{ Str::limit($post->body, 50) }}</p>
