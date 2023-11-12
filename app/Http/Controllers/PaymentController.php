1cadb68fdb8869ee559883f2dc96edf45aba40cb
tatsuya730 <t.taniguchi730@gmail>
2023-11-12 11:00:44 +0900

    決済後クレジットカードデータベースに反映

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; // ここでDBファサードをインポートします
 use Exception;
 
 class PaymentController extends Controller
@@ -19,17 +20,31 @@ class PaymentController extends Controller
     {
         \Stripe\Stripe::setApiKey(config('stripe.stripe_secret_key'));
 
-        try {
-            
+        DB::beginTransaction(); // トランザクションを開始するにはこの行を追加
 
-            \Stripe\Charge::create([
-                'source' => $request->stripeToken,
+        try {
+            $user = $request->user(); // 現在のログインユーザー
+
+            // StripeのChargeオブジェクトを作成して決済を実行
+            $charge = \Stripe\Charge::create([
                 'amount' => $request->amount,
                 'currency' => 'jpy',
+                'source' => $request->stripeToken,
+                'description' => 'コインの購入',
             ]);
+
+            if ($charge->paid) {
+                // コインの計算ロジック（例えば、100円で1コインとする）
+                $coinsToAdd = $request->amount / 100; // 1コインあたりの金額で割る
+                $user->coins += $coinsToAdd; // コインを追加
+                $user->save(); // ユーザー情報を保存
+            }
+
+            DB::commit(); // トランザクションをコミット
+            return back()->with('status', '決済が完了しました！');
         } catch (Exception $e) {
-            return back()->with('flash_alert', '決済に失敗しました！('. $e->getMessage() . ')');
+            DB::rollBack(); // エラーが発生したらロールバック
+            return back()->with('flash_alert', 'エラー: ' . $e->getMessage());
         }
-        return back()->with('status', '決済が完了しました！');
     }
 }
